6f5f975979c825ff8c9bc20b240c955717cabb8c
Belen Garcia <bbelengarcia6@gmail.com>
2021-04-20 17:59:58 +0200

    Eliminar duplicidad de la estructura whiel en varios métodos la clase Customer v23

---
+++ src/main/java/usantatecla/movies/v23/Customer.java
@@ -0,0 +1,46 @@
+package usantatecla.movies.v23;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+	
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		rentals = new ArrayList<Rental>();
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		Iterator<Rental> rentals = this.rentals.iterator();
+		String result = "Rental Record for " + this.getName() + "\n";
+		while (rentals.hasNext()) {
+			Rental each = rentals.next();
+			result += "\t" + each.getMovieTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+		}
+		result += "Amount owed is " + String.valueOf(this.getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		return this.rentals.stream().mapToDouble(Rental::getCharge).sum();
+	}
+	
+	private int getTotalFrequentRenterPoints() {
+		return this.rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+	}
+
+}
